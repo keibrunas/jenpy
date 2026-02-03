06eff707282b4daa62c86216c213e9930edc0ead
Samuele Bruna <s.bruna@keinavo.com>
2026-02-03 14:45:43 +0100

    merge conflict trial

--- app/demo_pipeline.py
+++ app/demo_pipeline.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
 """
 Demo pipeline for inserting data into BigQuery.
 Optimized for Container execution (UTC timestamps, Logging) and Testability.
 """
+
 # Comment: I like to argue with myself
 
 import datetime
